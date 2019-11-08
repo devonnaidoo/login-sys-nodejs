9390b0158bf8941525656af786597eadeb164f19
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-08 19:08:52 +0200

    register route updated

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ app.use(function(req, res, next) {
 // Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/register", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
