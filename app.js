29fe9039f4c36a7a50cc71e7f28543db0ad3f124
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-08 14:53:38 +0200

    middleware for express messages

--- app.js
+++ app.js
@@ -64,6 +64,13 @@ app.use(
   })
 );
 
+// Express messages middleware
+app.use(require("connect-flash")());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
