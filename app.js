8d286923f21f6199acf87f37d954eb597a1c1a25
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-08 15:08:13 +0200

    removed middleware for express validator

--- app.js
+++ app.js
@@ -44,26 +44,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Validator
-app.use(
-  expressValidator({
-    errorFormatter: function(param, msg, value) {
-      var namespace = param.split("."),
-        root = namespace.shift(),
-        formParam = root;
-
-      while (namespace.length) {
-        formParam += "[" + namespace.shift() + "]";
-      }
-      return {
-        param: formParam,
-        msg: msg,
-        value: value
-      };
-    }
-  })
-);
-
 // Express messages middleware
 app.use(require("connect-flash")());
 app.use(function(req, res, next) {
