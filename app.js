2cc0b9fb8eb7e694a4e7a7282140bbaefa9c0e10
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-08 14:32:03 +0200

    importing dependencies

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var bodyParser = require("body-parser");
-var 
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var multer = require("multer");
+var flash = require("connect-flash");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var db = mongoose.connection;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
